e0752b0ba8303951da4780c012661f5abe6b5495
CarleeMarie <97317700+CarleeMarie@users.noreply.github.com>
2022-08-13 22:07:16 -0500

    trying to add another jquery listener

--- js/index.js
+++ js/index.js
@@ -1,4 +1,26 @@
 console.log("Your index.js file is loaded correclty!!");
+$(document).ready(function() {
+  // grab the initial top offset of the navigation 
+     var stickyNavTop = $('.nav').offset().top;
+     
+     // our function that decides whether the navigation bar should have "fixed" css position or not.
+     var stickyNav = function(){
+      var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+           
+      // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+      // otherwise change it back to relative
+      if (scrollTop > stickyNavTop) { 
+          $('.nav').addClass('sticky');
+      } else {
+          $('.nav').removeClass('sticky'); 
+      }
+  };
+
+  stickyNav();
+  // and run it again every time you scroll
+  $(window).scroll(function() {
+    stickyNav();
+  });
 
 $(document).ready(function(){
     // Add smooth scrolling to all links
@@ -24,3 +46,4 @@ $(document).ready(function(){
       } // End if
     });
   });
+});
